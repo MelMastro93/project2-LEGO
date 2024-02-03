43a095aee3a168010c26b33c8ab2c09979c68bb8
Paolo <lupo.paolo96@gmail.com>
2024-02-03 15:00:13 +0100

    Script "yellow_nvb_search.js" updated and completed

--- scripts/navbars_scripts/yellow_nvb_search.js
+++ scripts/navbars_scripts/yellow_nvb_search.js
@@ -1,16 +1,29 @@
 const body = document.querySelector("body");
 const searchBtn = document.querySelector(".search_btn");
+const search_input = document.querySelector(".search_input");
 const closeBtn = document.querySelector(".close_btn");
-const searchInput = document.querySelectorAll(".search_input");
+
+function changeDisplay() {
+  let isShowed = search_input.style.display === "none";
+  let opacity = body.classList;
+  let showCloseBtn = closeBtn.style.display === "none";
+
+  if (isShowed && opacity.value === "" && showCloseBtn) {
+    search_input.style.display = "block";
+    opacity.value = "bodyOpacity";
+    closeBtn.style.display = "block";
+  } else {
+    search_input.style.display = "none";
+    search_input.removeAttribute("autofocus");
+    opacity.value = "";
+    closeBtn.style.display = "none";
+  }
+}
 
 searchBtn.addEventListener("click", () => {
-  body.classList.toggle("bodyOpacity");
+  changeDisplay();
 });
 
 closeBtn.addEventListener("click", (event) => {
-  searchInput.classList.toggle("search_input_close");
-});
-
-searchInput.addEventListener("click", (event) => {
-  searchInput.classList.toggle("search_input_close");
+  changeDisplay();
 });
