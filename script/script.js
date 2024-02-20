f3b87ab06910cb7939a6b320ddeee0ef2631fa68
Melissa Mastrovincenzo <melissa.mastrovincenzo@gmail.com>
2024-02-20 16:15:34 +0100

    button and slider fixed

--- script/script.js
+++ script/script.js
@@ -1,13 +1,28 @@
-let nextBtn = document.querySelector('.backBtn');
-let backBtn = document.querySelector('.nextBtn');
-let dragonCarousel =document.querySelector('.dragon_carousel');
+const initSlider = () => {
+    const arrows = document.querySelectorAll('.container_dragon_carousel .btn');
+    const sliderContainer = document.querySelector('.container_dragon_carousel .dragon_carousel')
+    
+    const maxScrollLeft = sliderContainer.scrollWidth - sliderContainer.clientWidth;
+    
+    arrows.forEach(button => {
+        button.addEventListener('click', ()=> {
+            const direction = button.id === "left" ? -1 : 1;
+            const scrollAmount = sliderContainer.clientWidth * direction;
+            sliderContainer.scrollBy({ left: scrollAmount, behavior: 'smooth'  })
+        })
+    })
+    
+    const handleSlideButtons = () => {
+        arrows[0].style.display = sliderContainer.scrollLeft <= 0 ? 'none' : 'block';
+        arrows[1].style.display = sliderContainer.scrollLeft >= maxScrollLeft ? 'none' : 'block';
+        
+    }
+    
+    sliderContainer.addEventListener('scroll', ()=> {
+        handleSlideButtons();
+    })
+    
+    }
 
-let currentIndex = 0
 
-backBtn.addEventListener('click', ()=>{
-    currentIndex = (currentIndex + 1 )  % dragonCarousel.children.length;
-})
-
-nextBtn.addEventListener('click', ()=>{
-    currentIndex = (currentIndex - 1 + dragonCarousel.children.length)  % dragonCarousel.children.length;
-})
+    window.addEventListener('load', initSlider)
