bc656e92070ebfb8dc4ea5276f4bc06179d873eb
Melissa Mastrovincenzo <melissa.mastrovincenzo@gmail.com>
2024-02-26 12:27:31 +0100

    javascript corrected

--- scripts/script_melissa/dragon.js
+++ scripts/script_melissa/dragon.js
@@ -1,25 +1,25 @@
 const initSlider = () => {
     const arrows = document.querySelectorAll('.container_dragon_carousel .btn');
-    const sliderContainer = document.querySelector('.container_dragon_carousel .dragon_carousel')
+    const dragonSliderContainer = document.querySelector('.container_dragon_carousel .dragon_carousel')
     
-    const maxScrollLeft = sliderContainer.scrollWidth - sliderContainer.clientWidth;
+    const maxScrollLeft = dragonSliderContainer.scrollWidth -dragonSliderContainer.clientWidth;
     
     arrows.forEach(button => {
         button.addEventListener('click', ()=> {
             const direction = button.id === "left" ? -1 : 1;
-            const scrollAmount = sliderContainer.clientWidth * direction;
-            sliderContainer.scrollBy({ left: scrollAmount, behavior: 'smooth'  })
+            const scrollAmount = dragonSliderContainer.clientWidth * direction;
+            dragonSliderContainer.scrollBy({ left: scrollAmount, behavior: 'smooth'  })
         })
     })
     
-    const handleSlideButtons = () => {
-        arrows[0].style.display = sliderContainer.scrollLeft <= 0 ? 'none' : 'block';
-        arrows[1].style.display = sliderContainer.scrollLeft >= maxScrollLeft ? 'none' : 'block';
+    const handleDragonSlideButtons = () => {
+        arrows[0].style.display = dragonSliderContainer.scrollLeft <= 0 ? 'none' : 'block';
+        arrows[1].style.display = dragonSliderContainer.scrollLeft >= maxScrollLeft ? 'none' : 'block';
         
     }
     
-    sliderContainer.addEventListener('scroll', ()=> {
-        handleSlideButtons();
+    dragonSliderContainer.addEventListener('scroll', ()=> {
+        handleDragonSlideButtons();
     })
     
     }
