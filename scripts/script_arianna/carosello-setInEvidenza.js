5759fd7d2506e7ae50dd1aa3214eec4c8ea945b2
ariannagiaccari <ariggiaccari@gmail.com>
2024-02-27 16:20:40 +0100

    ari-section-mediaqueries

--- scripts/script_arianna/carosello-setInEvidenza.js
+++ scripts/script_arianna/carosello-setInEvidenza.js
@@ -1,6 +1,7 @@
+// second carousel
 const initSlider = () => {
 const arrows = document.querySelectorAll('.slider-set .button');
-const sliderContainer = document.querySelector('.slider-set .card-set')
+const sliderContainer = document.querySelector('.slider-set .card-set');
 
 const maxScrollLeft = sliderContainer.scrollWidth - sliderContainer.clientWidth;
 
@@ -14,18 +15,41 @@ arrows.forEach(button => {
 
 const handleSlideButtons = () => {
     arrows[0].style.display = sliderContainer.scrollLeft <= 0 ? 'none' : 'block';
-    arrows[1].style.display = sliderContainer.scrollLeft >= maxScrollLeft ? 'none' : 'block';
-    
+    arrows[1].style.display = sliderContainer.scrollLeft >= maxScrollLeft ? 'none' : 'block';   
 }
 
 sliderContainer.addEventListener('scroll', ()=> {
     handleSlideButtons();
 })
-
 }
 
-
 window.addEventListener('load', initSlider)
+
+// first carousel
+const initSlider2 = () => {
+    const arrows2 = document.querySelectorAll('.whole-first-sec .secondarrows');
+    const weekFavsContainer = document.querySelector('.whole-first-sec .compra-ora-3images');
+    const maxScrollLeft2 = weekFavsContainer.scrollWidth - weekFavsContainer.clientWidth;
+    
+    arrows2.forEach(arrow => {
+        arrow.addEventListener('click', ()=> {
+            const arrowDirection = arrow.id === "left-arrow" ? -1 : 1;
+            const scroll = weekFavsContainer.clientWidth * arrowDirection;
+            weekFavsContainer.scrollBy({ left: scroll, behavior: 'smooth'  })
+        })
+    })
+    
+    const handleSlideButtons2 = () => {
+        arrows2[0].style.display = weekFavsContainer.scrollLeft <= 0 ? 'none' : 'block';
+        arrows2[1].style.display = weekFavsContainer.scrollLeft >= maxScrollLeft2 ? 'none' : 'block';   
+    }
+    
+    weekFavsContainer.addEventListener('scroll', ()=> {
+        handleSlideButtons2();
+    })
+    }
+    
+    window.addEventListener('load', initSlider2)
 
 
 
