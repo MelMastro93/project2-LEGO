84260b8a5ca1b3baa85df79f2cb8c499c8f3602f
valentina Li <valentinalidev@gmail.com>
2024-02-28 15:15:48 +0100

    fixed button dragon section

--- scripts/script_melissa/dragon.js
+++ scripts/script_melissa/dragon.js
@@ -1,12 +1,12 @@
 const initDragonSlider = () => {
-    const dragon_arrows = document.querySelectorAll('.container_dragon_carousel .btn');
+    const dragon_arrows = document.querySelectorAll('.container_dragon_carousel .btn_dragon_slide');
     const dragonSliderContainer = document.querySelector('.container_dragon_carousel .dragon_carousel')
     
     const maxScrollLeft = dragonSliderContainer.scrollWidth -dragonSliderContainer.clientWidth;
     
     dragon_arrows.forEach(button => {
         button.addEventListener('click', ()=> {
-            const direction = button.id === "left" ? -1 : 1;
+            const direction = button.id === "prev_slide_dragon" ? -1 : 1;
             const scrollAmount = dragonSliderContainer.clientWidth * direction;
             dragonSliderContainer.scrollBy({ left: scrollAmount, behavior: 'smooth'  })
         })
