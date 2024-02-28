2e710911b01798cd40185c7f8ad7d5e593120fb6
Paolo <lupo.paolo96@gmail.com>
2024-02-28 12:49:54 +0100

    Updated navbar scripts

--- scripts/script_paolo/yellow_nvb.js
+++ scripts/script_paolo/yellow_nvb.js
@@ -5,14 +5,34 @@ const leftElements = document.querySelector(".yellow_navbar_left");
 const searchBtn = document.querySelector(".search_btn");
 const search_input = document.querySelector(".search_input");
 const closeBtn = document.querySelector(".close_btn");
+const logo = document.querySelector("#lego_nvb_logo");
 
 //SEARCHBAR'S FUNC
 function changeDisplay() {
   let isShowed = search_input.style.display === "none";
   let opacity = body.classList;
   let showCloseBtn = closeBtn.style.display === "none";
+  let smartphoneQueries = window.matchMedia("(max-width: 430px)");
+  let tabletQueries = window.matchMedia("(max-width: 768px)");
 
-  if (isShowed && opacity.value === "" && showCloseBtn) {
+  if (
+    smartphoneQueries.matches &&
+    isShowed &&
+    opacity.value === "" &&
+    showCloseBtn
+  ) {
+    leftElements.style.marginRight = "0";
+    search_input.style.display = "block";
+    search_input.style.width = "255px";
+    opacity.value = "bodyOpacity";
+    closeBtn.style.display = "block";
+    logo.style.display = "none";
+  } else if (
+    tabletQueries.matches &&
+    isShowed &&
+    opacity.value === "" &&
+    showCloseBtn
+  ) {
     leftElements.style.marginRight = "0";
     search_input.style.display = "block";
     opacity.value = "bodyOpacity";
@@ -22,6 +42,7 @@ function changeDisplay() {
     search_input.style.display = "none";
     opacity.value = "";
     closeBtn.style.display = "none";
+    logo.style.display = "block";
   }
 }
 
