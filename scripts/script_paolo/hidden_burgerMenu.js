4ffe73d1458e8fb8c54619ea0c5378df44555ae1
Paolo <lupo.paolo96@gmail.com>
2024-02-29 12:38:03 +0100

    Added hamburger sub-menu futures

--- scripts/script_paolo/hidden_burgerMenu.js
+++ scripts/script_paolo/hidden_burgerMenu.js
@@ -7,6 +7,111 @@ const burgerMenu = document.querySelector(
 const primaryBurgerToggle = document.querySelector(".burger_menu_Btn_1");
 const secondaryBurgerToggle = document.querySelector("burger_menu_Btn_2");
 
+// HAMBURGER PRIMARY MENU IDENTIFIER
+
+const hamburgerBuyBtn = document.querySelector("#hamburger_buy_btn");
+const hamburgerDiscoverBtn = document.querySelector("#hamburger_discover_btn");
+const hamburgerHelpBtn = document.querySelector("#hamburger_help_btn");
+
+const hamburgerBuyMenu = document.querySelector(".hamburger_menu_buy");
+const hamburgerDiscoverMenu = document.querySelector(
+  ".hamburger_menu_discover"
+);
+const hamburgerHelpMenu = document.querySelector(".hamburger_menu_help");
+
+// HAMBURGER SECONDARY MENU IDENTIFIER
+
+const getBackBtn = document.querySelector(".burgerHiddenMenuButton");
+const getBackBtnSVG = document.querySelector(".burgerHiddenMenuButtonSVG");
+
+// HAMBURGER SECONDARY MENU FUNCTION
+
+function getBackToMenu() {
+  let hamBuyMenuDisplay = hamburgerBuyMenu.style.display === "block";
+  let hamDiscoverMenuDisplay = hamburgerDiscoverMenu.style.display === "block";
+  let hamHelpMenuDisplay = hamburgerHelpMenu.style.display === "block";
+
+  if (hamBuyMenuDisplay || hamDiscoverMenuDisplay || hamHelpMenuDisplay) {
+    hamburgerBuyMenu.style.display = "none";
+    hamburgerDiscoverMenu.style.display = "none";
+    hamburgerHelpMenu.style.display = "none";
+    getBackBtnSVG.style.display = "none";
+  } else {
+    hamburgerBuyMenu.style.display = "none";
+    hamburgerDiscoverMenu.style.display = "none";
+    hamburgerHelpMenu.style.display = "none";
+    getBackBtnSVG.style.display = "block";
+  }
+}
+
+getBackBtn.addEventListener("click", () => {
+  getBackToMenu();
+});
+
+// HAMBURGER PRIMARY MENU FUNCTION
+
+function openHamBuyMenu() {
+  let hamBuyMenuDisplay = hamburgerBuyMenu.style.display === "none";
+  let getBackBtnSVGDisplay = getBackBtnSVG.style.display === "none";
+
+  if (hamBuyMenuDisplay && getBackBtnSVGDisplay) {
+    hamburgerBuyMenu.style.display = "block";
+    hamburgerDiscoverMenu.style.display = "none";
+    hamburgerHelpMenu.style.display = "none";
+    getBackBtnSVG.style.display = "block";
+  } else {
+    hamburgerBuyMenu.style.display = "none";
+    hamburgerDiscoverMenu.style.display = "none";
+    hamburgerHelpMenu.style.display = "none";
+    getBackBtnSVG.style.display = "none";
+  }
+}
+
+function openHamDiscoverMenu() {
+  let hamDiscoverMenuDisplay = hamburgerDiscoverMenu.style.display === "none";
+  let getBackBtnSVGDisplay = getBackBtnSVG.style.display === "none";
+
+  if (hamDiscoverMenuDisplay && getBackBtnSVGDisplay) {
+    hamburgerBuyMenu.style.display = "none";
+    hamburgerDiscoverMenu.style.display = "block";
+    hamburgerHelpMenu.style.display = "none";
+    getBackBtnSVG.style.display = "block";
+  } else {
+    hamburgerBuyMenu.style.display = "none";
+    hamburgerDiscoverMenu.style.display = "none";
+    hamburgerHelpMenu.style.display = "none";
+    getBackBtnSVG.style.display = "none";
+  }
+}
+
+function openHamHelpMenu() {
+  let hamHelpMenuDisplay = hamburgerHelpMenu.style.display === "none";
+  let getBackBtnSVGDisplay = getBackBtnSVG.style.display === "none";
+
+  if (hamHelpMenuDisplay && getBackBtnSVGDisplay) {
+    hamburgerBuyMenu.style.display = "none";
+    hamburgerDiscoverMenu.style.display = "none";
+    hamburgerHelpMenu.style.display = "block";
+    getBackBtnSVG.style.display = "block";
+  } else {
+    hamburgerBuyMenu.style.display = "none";
+    hamburgerDiscoverMenu.style.display = "none";
+    hamburgerHelpMenu.style.display = "none";
+    getBackBtnSVG.style.display = "none";
+  }
+}
+
+hamburgerBuyBtn.addEventListener("click", () => {
+  openHamBuyMenu();
+});
+
+hamburgerDiscoverBtn.addEventListener("click", () => {
+  openHamDiscoverMenu();
+});
+
+hamburgerHelpBtn.addEventListener("click", () => {
+  openHamHelpMenu();
+});
 // HAMBURGER TOGGLE FUNCTION
 
 function handleBurgerMenuDisplay() {
